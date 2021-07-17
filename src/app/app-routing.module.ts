6f32ca445aa0705d5d84c672a3216aaac444b4dc
Mehmet Aziz Algüllü <3363217433MAziz28666774646>
2021-07-17 09:39:55 +0300

    Added Login Form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 import { ProductAddComponent } from './components/product-add/product-add.component';
 import { ProductComponent } from './components/product/product.component';
 
@@ -7,7 +8,8 @@ const routes: Routes = [
   {path:"", component:ProductComponent},
   {path:"products",pathMatch:"full", component:ProductComponent},//birisi www.mehmetazizalgullu.com/products derse de yine product componenti göster.
   {path:"products/category/:categoryID", component:ProductComponent},
-  {path:"products/add", component:ProductAddComponent}
+  {path:"products/add", component:ProductAddComponent},
+  {path:"login", component:LoginComponent}
 ];
 
 @NgModule({
