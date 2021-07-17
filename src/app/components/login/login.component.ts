31a0857e3b4363b42b79816446b3b195949ef621
Mehmet Aziz Algüllü <3363217433MAziz28666774646>
2021-07-17 13:27:52 +0300

    Added SingleResponseModel, A little refactoring

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -10,7 +13,7 @@ export class LoginComponent implements OnInit {
 
   loginForm:FormGroup;
 
-  constructor(private formBuilder:FormBuilder) { }
+  constructor(private formBuilder:FormBuilder, private authService:AuthService, private toastrService:ToastrService, private routerService:Router) { }
 
   ngOnInit(): void {
     this.createLoginForm();
@@ -25,7 +28,15 @@ export class LoginComponent implements OnInit {
 
   login() {
     if (this.loginForm.valid) {
-      
+      let loginModel = Object.assign({}, this.loginForm.value);
+      this.authService.login(loginModel).subscribe(response=>{
+        this.toastrService.success("Giriş başarılı!", "Bilgilendirme");
+        //not: localstorage'i servise taşısan daha iyi olur.
+        localStorage.setItem("token", response.data.token.toString());
+        this.routerService.navigateByUrl("/products");
+      }, responseError=> {
+        
+      })
     }
   }
 
